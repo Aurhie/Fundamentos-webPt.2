03d1deacdb3e242a1b68fd7092724a19871e76c5
Aurora Marques <Auroras321@hotmail.com>
2022-03-29 17:20:32 -0300

    tabPostagem

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import {Typography, Box, Grid, Button} from '@material-ui/core';
 import './Home.css';
+import TabPostagem from '../../componentes/postagens/tabpostagem/TabPostagem';
 import rohan from '../../assets/img/rohan.png'
 
-function Home()
-{
-    return(
+function Home() {
+    return (
         <>
-         <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
                 <Grid alignItems="center" item xs={6}>
                     <Box paddingX={20} >
                         <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Seja bem vindo(a)!</Typography>
@@ -21,8 +21,9 @@ function Home()
                 </Grid>
                 <Grid item xs={6} >
                     <img src={rohan} alt="" width="500px" height="500px" />
-                </Grid>
+                    </Grid>
                 <Grid xs={12} className='postagens'>
+                    <TabPostagem />
                 </Grid>
             </Grid>
         </>
--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -16,33 +16,32 @@ function Login() {
             senha: '',
             token: ''
         }
-    )
+        )
 
-    function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+        function updatedModel(e: ChangeEvent<HTMLInputElement>) {
 
-        setUserLogin({
-            ...userLogin,
-            [e.target.name]: e.target.value
-        })
-    }
-
-    useEffect(() => {
-        if (token != '') {
-            history.push('/home')
+            setUserLogin({
+                ...userLogin,
+                [e.target.name]: e.target.value
+            })
         }
-    }, [token])
 
-    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
-        e.preventDefault();
-        try {
-            await login(`/usuarios/logar`, userLogin, setToken)
+            useEffect(()=>{
+                if(token != ''){
+                    history.push('/home')
+                }
+            }, [token])
 
-            alert('Usuário logado com sucesso!');
+        async function onSubmit(e: ChangeEvent<HTMLFormElement>){
+            e.preventDefault();
+            try{
+                await login(`/usuarios/logar`, userLogin, setToken)
+
+                alert('Usuário logado com sucesso!');
+            }catch(error){
+                alert('Dados do usuário inconsistentes. Erro ao logar!');
+            }
         }
-        catch (error) {
-            alert('Dados incorretos. Não foi posssível logar.')
-        }
-    }
 
     return (
         <Grid container direction='row' justifyContent='center' alignItems='center'>
@@ -50,14 +49,14 @@ function Login() {
                 <Box paddingX={20}>
                     <form onSubmit={onSubmit}>
                         <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
-                        <TextField value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
-                        <TextField value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                        <TextField value={userLogin.usuario} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField value={userLogin.senha} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password'fullWidth />
                         <Box marginTop={2} textAlign='center'>
-                            <Button type='submit' variant='contained' color='primary'>
-                                Logar
-                            </Button>
+                                <Button type='submit' variant='contained' color='primary'>
+                                    Logar
+                                </Button>
                         </Box>
-                        </form>
+                    </form>
                     <Box display='flex' justifyContent='center' marginTop={2}>
                         <Box marginRight={1}>
                             <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
